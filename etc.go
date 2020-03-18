84a408a045a448b7c97bdd09fde8cb505d095e48
Rob MacInnis <rob@bakerstreet.industries>
2020-03-17 21:31:29 -0400

    Adding etc methods

--- etc.go
+++ etc.go
@@ -1,8 +1,13 @@
 package util
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -49,4 +54,30 @@ func BuildSelf(path string) string {
 
 	self = self + "/" + Config.URLVersionPrefix() + "/" + path
 	return self
+}
+
+// WaitForShutdown waits for shutdown from the http server
+func WaitForShutdown(srv *http.Server) {
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block until we receive our signal.
+	<-interruptChan
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	srv.Shutdown(ctx)
+
+	log.Println("Shutting down")
+	os.Exit(0)
+}
+
+// GetEnv gets an environment variable
+func GetEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
 }
